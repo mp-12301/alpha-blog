933764313c4e8ed007f2aea456b4cc67a8dd6b70
Marcio Pereira <marcio@gaspardbruno.com>
2021-09-15 15:26:01 +0100

    added users create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+  before_save { self.email = email.downcase }
   validates :username, presence: true, 
       uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
 
@@ -8,5 +9,7 @@ class User < ApplicationRecord
   validates :email, presence: true, 
       uniqueness: { case_sensitive: false }, length: { maximum: 105 },
       format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "about", to: 'pages#about'
 
+  get "signup", to: 'users#new'
+  resources :users, except: [:new]
   resources :articles
 end
