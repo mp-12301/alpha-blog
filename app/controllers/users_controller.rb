2c491b3624d0650698e8217279cdf006e781a7c6
Marcio Pereira <marcio@gaspardbruno.com>
2021-10-04 10:53:28 +0100

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if current_user == @user
     flash[:notice] = "Account and all associated articles successfully deleted" 
     redirect_to articles_path
   end
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   end
 
   def same_user 
-    if @user != current_user
+    if @user != current_user && !current_user.admin?
       flash[:alert] = "You can only edit your own profile"
       redirect_to @user
     end
